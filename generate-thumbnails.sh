97a1d5dfda5107f1a01183bed2767db8aef783aa
Chris Hager <chris@linuxuser.at>
2020-01-09 15:44:02 +0100

    better blinkenrocket image

--- generate-thumbnails.sh
+++ generate-thumbnails.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 mkdir -p images/projects
-magick mogrify -resize 1024x683 -quality 100 -path images/projects/ images_raw/projects/*.jpg
-magick mogrify -resize 1024x683 -quality 100 -path images/projects/ images_raw/projects/*.jpeg
-magick mogrify -resize 1024x683 -quality 100 -path images/projects/ images_raw/projects/*.png
+
+if [ -z "$1" ]; then
+    magick mogrify -resize 1024x683 -quality 100 -path images/projects/ images_raw/projects/*.jpg
+    magick mogrify -resize 1024x683 -quality 100 -path images/projects/ images_raw/projects/*.jpeg
+    magick mogrify -resize 1024x683 -quality 100 -path images/projects/ images_raw/projects/*.png
+else
+    echo "Optimizing $1"
+    magick mogrify -resize 1024x683 -quality 100 -path images/projects/ $1
+fi
